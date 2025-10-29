130ead2330f8a8dea893f3d0612b412511aeb38d
specialwan <wawannaufal77@gmail.com>
2025-10-30 02:57:23 +0700

    simulasi MPC raspi

--- mpc_simulation_standalone.py
+++ mpc_simulation_standalone.py
@@ -29,7 +29,7 @@ from typing import List, Tuple
 class SimulationConfig:
     """Konfigurasi simulasi"""
     # MPC parameters
-    dt: float = 0.05         # Sample time (s) - 10 Hz
+    dt: float = 0.1         # Sample time (s) - 10 Hz
     Np: int = 6              # Prediction horizon
     Nc: int = 3              # Control horizon
     
